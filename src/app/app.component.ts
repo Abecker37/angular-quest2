73446e9bf1b1df061c72b36dae0dd274609e0bb7
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2024-01-15 12:10:32 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { BlockComponent } from './block/block.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
